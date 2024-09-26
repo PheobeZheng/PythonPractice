bafbd61b59a951151aa9ea4718a77cd2119ccdaa
ahui <bo_li_xie@126.com>
2024-09-26 15:52:23 +0800

    去掉没用的代码（左对齐版本）

--- 99table.py
+++ 99table.py
@@ -7,12 +7,3 @@ for i in range(1, 10):
         print(f'{i} * {j} = {i * j}', end='\t')
         if j == 9:
             print(end='\n')
-
-
-print('Hello world!')
-print('九九乘法表：')
-for i in range(1, 10):
-    for j in range(i, 10):
-        print(f'{i} * {j} = {i * j}', end='\t')
-        if j == 9:
-            print(end='\n')
